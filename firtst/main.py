3566b54a1587fdd3f22a4a6a6e623509c5eadea9
vvnbel <vvnbel@gmail.com>
2021-09-05 10:32:21 +0300

    init

---
+++ firtst/main.py
@@ -0,0 +1,9 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+
+class MoneyAccountApp(MDApp):
+    def build(self):
+        return MDLabel(text="HELO0", halign="center")
+
+MoneyAccountApp().run()
